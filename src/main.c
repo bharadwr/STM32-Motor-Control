2e1519636041dbb8c6911a468f7dc7e919ad5beb
mgulia <46496494+mgulia@users.noreply.github.com>
2019-04-18 02:31:48 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include "stm32f0xx.h"
 #include "stm32f0_discovery.h"
 #include <stdlib.h>
+#include <string.h>
+uint32_t dist_sensor1 = 0;
+uint32_t dist_sensor2 = 0;
+int count = 0;
+int distance = 0;
+int high_low = 0;
 
 #define COL1 (1 << 0)
 #define COL2 (1 << 1)
@@ -24,13 +30,15 @@
 void init_lcd(void);
 void display1(const char *);
 void display2(const char *);
-
+float travelled_distance;
 int col = 0;
 int row = -1;
 int red = 0, blue = 0, grn = 0;
+int runflag = 0;
 int speed = 0;
 int fb = 0;
 int lr = 0;
+int test_dist;
 char char_array[4][4] = { {'1', '2', '3', 'A'},
 		{'4', '5', '6', 'B'},
 		{'7', '8', '9', 'C'},
@@ -111,22 +119,149 @@ void init_pwm1(void)
 	TIM1->CR1 |= TIM_CR1_CEN;
 }
 
+//CODE FROM SENSORS
+void ultrasonic_setup_10_11(){
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+
+	//change to 8 and 9
+
+	//OUTPUT MODE FOR GPIOC           //TRIG PC4
+	GPIOC->MODER &= ~(0X3 << (11 * 2)); //ORGINIALLY PB5
+	GPIOC->MODER |=  (0X1 << (11 * 2));
+	//PUSH-PULL PUSH PULL
+	GPIOC->OTYPER &= ~(0X1 << 11);
+	//OSPEEDR
+	GPIOC->OSPEEDR |= (0X3 << (11 * 2));
+
+
+	//INPUT MODE FOR GPIOA             //ECHO PC3
+	GPIOC->MODER &= ~(0X3 << (10 * 2)); //ORGINALLY PA3
+	//A PUPDR
+	GPIOC->PUPDR &= ~(0X3 << (10 * 2));
+	GPIOC->OSPEEDR |= (0X3 << (10 * 2));
+}
+
+//PC3 AND PC4 FOR ULTRA34
+void ultrasonic_10_11(){
+
+	unsigned char i;
+	unsigned long int j;
+	float Ultr_Temp = 0;
+	nano_wait(100000000);
+	for (i = 0; i < 5; i++)
+	{
+		GPIOC->BSRR |= GPIO_BSRR_BS_11;
+		//DELAY *
+		nano_wait(100000);
+
+		GPIOC->BSRR |= GPIO_BSRR_BR_11;
+
+		while((GPIOC->IDR & GPIO_IDR_10) == 0);
+
+		while(GPIOC->IDR & GPIO_IDR_10) {
+			//DELAY **
+			nano_wait(8000);
+			j++;
+		}
+
+		Ultr_Temp += 340/2*j*10;
+		j=0;
+		//DELAY *
+		nano_wait(10000);
+
+	}
+
+	int dist = Ultr_Temp/50000;
+	dist_sensor1 = dist;
+	char buf[20];
+	itoa(dist, buf, 10);
+	//display1("test1");
+
+	if (dist >= 0){
+		//display1(buf);
+	}
+
+}
+
+void hall_setup12(){
+	//RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+
+	//change to 8 and 9
+
+	//OUTPUT MODE FOR GPIOC
+	GPIOC->MODER &= ~(0X3 << (12 * 2));
+	GPIOC->MODER |=  (0x0 << (12 * 2));
+	//PUSH-PULL PUSH PULL
+	GPIOC->OTYPER &= ~(0X1 << 12);
+	//OSPEEDR
+	GPIOC->OSPEEDR |= (0X3 << (12 * 2));
+	GPIOC->PUPDR &= ~(0X3 << (12 * 2));
+	GPIOC->PUPDR |= (0X1 << (12 * 2));
+}
+
+void TIM15_IRQHandler(void) {
+	TIM15->SR = ~TIM_SR_UIF;
+	//display1("Here");
+	int circumference = 20; //cm
+	char snum[16];
+	//display2("testing");
+
+	if (GPIOC->IDR & GPIO_IDR_12) {
+		//display1("High");
+		high_low = 0;
+	}
+	else {
+		//display1("Low");
+		if (high_low == 0)
+		{
+			travelled_distance += circumference/5;
+			if (travelled_distance >= test_dist) {
+				runflag = 1;
+				TIM15->CR1 &= ~TIM_CR1_CEN;
+			}
+		}
+		high_low = 1;
+		//display2("Low");
+	}
+	itoa(travelled_distance, snum, 10);
+	display2(snum);
+	ultrasonic_10_11();
+
+
+	count += 1;
+	//if ((dist_sensor1 >= 2000 |dist_sensor2 >= 2000 | dist_sensor1 <= 20 | dist_sensor2 <= 20) & count > 5){
+	if ((dist_sensor1 >= 2000 | dist_sensor1 <= 20) & count > 5){
+		display1("Run obstructed");
+		display2("Press reset");
+		TIM1->CCR1 = 0;
+		TIM1->CCR2 = 0;
+		exit(0);
+	}
+}
+
+void tim15_init(void){
+	RCC->APB2ENR |= RCC_APB2ENR_TIM15EN;
+	TIM15->PSC = 4800 - 1;
+	TIM15->ARR = 100 - 1;
+	TIM15->DIER |= TIM_DIER_UIE;
+	NVIC->ISER[0] = 1<<TIM15_IRQn;
+	TIM15->CR1 |= TIM_CR1_CEN;
+}
+
 void F(int distance)
 {
-	TIM1->CCR1 = 49 + 10 * speed;
-	TIM1->CCR2 = 49 + 10 * speed;
-
-	GPIOA->BSRR |= 1<<5 | 1<<7;
-	GPIOA->BRR |= 1<<4 | 1<<6;
-
-	for(int i=0; i<distance; i++) {
-		/*if (read the hall value is not 0)
-		 *	increment distance
-		 *if (distance >= F/B distance)
-		 *	break
-		 */
-		nano_wait(250*1000*1000);//enter time to go forward
-	}
+	TIM1->CCR1 = 20 + 10 * speed;
+	TIM1->CCR2 = 20 + 10 * speed;
+	GPIOA->BSRR |= 1<<4 | 1 << 6;
+	GPIOA->BRR |= 1<<5 | 1<<7 ;
+	int count = 0;
+	tim15_init();
+	test_dist = distance;
+	runflag = 0;
+	travelled_distance = 0;
+	while(!runflag);
+	TIM15->CR1 &= ~TIM_CR1_CEN;
+	runflag = 0;
 	TIM1->CCR1 = 0;
 	TIM1->CCR2 = 0;
 	GPIOA->BRR |= 1<<4 | 1<<5 | 1<<6 | 1<<7;
@@ -134,23 +269,33 @@ void F(int distance)
 
 void B(int distance)
 {
-	TIM1->CCR1 = 49 + 10 * speed;
-	TIM1->CCR2 = 49 + 10 * speed;
+	TIM1->CCR1 = 20 + 10 * speed;
+	TIM1->CCR2 = 20 + 10 * speed;
+	GPIOA->BSRR |= 1<<5 | 1<<7;
+	GPIOA->BRR |= 1<<4 | 1<<6;
+	int count = 0;
+	tim15_init();
+	test_dist = distance;
+	runflag = 0;
+	travelled_distance = 0;
+	while(!runflag);
+	TIM15->CR1 &= ~TIM_CR1_CEN;
+	runflag = 0;
+	TIM1->CCR1 = 0;
+	TIM1->CCR2 = 0;
+	GPIOA->BRR |= 1<<4 | 1<<5 | 1<<6 | 1<<7;
+}
 
-	GPIOA->BSRR |= 1<<4 | 1 << 6;
-	GPIOA->BRR |= 1<<5 | 1<<7 ;
+void L(void)
+{
+	TIM1->CCR1 = 44;
+	TIM1->CCR2 = 44;
 
-	for(int i=0; i<distance; i++) {
-		/*if (read the hall value is not 0)
-		 *	increment distance
-		 *if (distance >= F/B distance)
-		 *	break
-		 */
-		/* if (ir is high)
-		 * 	exit(0);
-		 */
+	GPIOA->BSRR |= 1<<5 | 1<<6;
+	GPIOA->BRR |= 1<<4 | 1<<7;
+
+	for(int i=0; i<5; i++)
 		nano_wait(250*1000*1000);//enter time to go forward
-	}
 
 	TIM1->CCR1 = 0;
 	TIM1->CCR2 = 0;
@@ -159,23 +304,8 @@ void B(int distance)
 
 void R(void)
 {
-	TIM1->CCR1 = 75;
-	TIM1->CCR2 = 75;
-
-	GPIOA->BSRR |= 1<<5 | 1<<6;
-	GPIOA->BRR |= 1<<4 | 1<<7;
-
-	for(int i=0; i<5; i++)
-		nano_wait(250*1000*1000);//enter time to go forward
-	TIM1->CCR1 = 0;
-	TIM1->CCR2 = 0;
-	GPIOA->BRR |= 1<<4 | 1<<5 | 1<<6 | 1<<7;
-}
-
-void L(void)
-{
-	TIM1->CCR1 = 75;
-	TIM1->CCR2 = 75;
+	TIM1->CCR1 = 44;
+	TIM1->CCR2 = 44;
 
 	GPIOA->BSRR |= 1<<4 | 1<<7;
 	GPIOA->BRR |= 1<<5 | 1<<6;
@@ -253,9 +383,9 @@ void get_coordinates() {
 		if(key != '\0') {
 			if (key == 'D') {
 				if (line1[2] != '-')
-					line1[2] = '-';
+					line1[2] = ' ';
 				else
-					line1[2] = '+';
+					line1[2] = ' ';
 			}
 			else {
 				switch(key) {
@@ -370,11 +500,11 @@ void setup_timer3() {
 
 }
 
-// Use the global variable ‘col’ provided in the skeleton file,
-// increment it by 1. Check if ‘col’ exceeds 3, if so, reset it to 0.
-// This is because we have 4 columns on the keypad and ‘col’ dictates which
+// Use the global variable â€˜colâ€™ provided in the skeleton file,
+// increment it by 1. Check if â€˜colâ€™ exceeds 3, if so, reset it to 0.
+// This is because we have 4 columns on the keypad and â€˜colâ€™ dictates which
 // column is currently being driven. Set the output data register
-// of the port A to (1 << col). This drives the corresponding column to ‘logic 1’.
+// of the port A to (1 << col). This drives the corresponding column to â€˜logic 1â€™.
 void TIM3_IRQHandler()
 {
 	int current_row = -1;
@@ -418,24 +548,28 @@ int main(void)
 	display2("# for speed");
 
 	char indicator = get_char_key();
-	int complete = 0;
-	while(complete != 2) {
+	int complete1 = 1;
+	int complete2 = 1;
+	while(complete1 || complete2) {
 		// Complete the code here in this loop...
 		indicator = get_char_key();
 		if (indicator ==  '*')
 		{
 			get_coordinates();
-			complete++;
+			complete1 = 0;
 		}
 		if (indicator ==  '#')
 		{
 			get_speed();
-			complete++;
+			complete2 = 0;
 		}
 	}
+	nano_wait(1000000000);
 	init_pwm1();
 	init_direcX();
 	initPA0();
+	ultrasonic_setup_10_11();
+	hall_setup12();
 	display1("Starting.");
 	display2("");
 	nano_wait(1000000000);
@@ -457,13 +591,14 @@ int main(void)
 		B(-fb);
 	}
 	nano_wait(10000000000);
-	if (lr < 0) {
+	if (lr <= 0) {
 		itoa(lr, snum, 10);
 		display1("Left");
 		display2(snum);
 		L();
 		nano_wait(1000000000);
-		F(-lr);
+		if (lr != 0)
+			F(-lr);
 	}
 	else if (lr > 0) {
 		itoa(lr, snum, 10);
@@ -471,7 +606,8 @@ int main(void)
 		display2(snum);
 		R();
 		nano_wait(1000000000);
-		F(lr);
+		if (lr != 0)
+			F(lr);
 	}
 	nano_wait(10000000000);
 	testLED();
